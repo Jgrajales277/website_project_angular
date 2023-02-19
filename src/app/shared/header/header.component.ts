c0207641af5e28c4ba1b7444d0d1ed210e4c4cf3
“Juliana <jgrajales277@gmail.com>
2023-02-19 16:06:38 -0500

    Crear interfaz para reemplazar la información de la páginay agregar animación en imagenes de las páginas de  about y de home

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InfoPagina } from '../../interfaces/info-pagina-interface';
+import { InfoPaginaService } from '../../services/info-paginas.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  constructor( public _servicio: InfoPaginaService ) {}
+
+  ngOnInit() {
+  
+  }
 
 }
+
+  
